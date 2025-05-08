aced011408e2533d7b952c347f4190c47a763c2d
Madeliy Ricra Gutierrez <madeliy.r@wearesolu.com>
2025-05-08 00:33:12 -0500

    [bugfix/checkout]: Fix remove function

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useState, type Ref } from "react";
+import { forwardRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import "./styles.css";
 import type { ICheckoutProps, IFieldProps } from "./props";
